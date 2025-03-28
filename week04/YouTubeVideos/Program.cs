6ae5a789fa68f46debd529585da213c5d27ac60c
johnanemba <johnanemba@gmail.com>
2025-03-28 20:34:00 +0300

    a

--- week04/YouTubeVideos/Program.cs
+++ week04/YouTubeVideos/Program.cs
@@ -1,9 +1,48 @@
 using System;
+using System.Collections.Generic;
 
-class Program
+class Video
 {
-    static void Main(string[] args)
+    public string Title { get; set; }
+    public string Author { get; set; }
+    public int Duration { get; set; } // Duration in seconds
+    private List<Comment> Comments { get; set; }
+
+    public Video(string title, string author, int duration)
     {
-        Console.WriteLine("Hello World! This is the YouTubeVideos Project.");
+        Title = title;
+        Author = author;
+        Duration = duration;
+        Comments = new List<Comment>();
+    }
+    
+    public class Comment
+    {
+        public string Author { get; set; }
+        public string Text { get; set; }
+    
+        public Comment(string author, string text)
+        {
+            Author = author;
+            Text = text;
+        }
+    }
+
+    public void AddComment(Comment comment)
+    {
+        Comments.Add(comment);
+    }
+
+    public void ShowDetails()
+    {
+        Console.WriteLine($"Title: {Title}");
+        Console.WriteLine($"Author: {Author}");
+        Console.WriteLine($"Duration: {Duration} seconds");
+        Console.WriteLine("Comments:");
+        foreach (var comment in Comments)
+        {
+            Console.WriteLine($"- {comment.Author}: {comment.Text}");
+        }
+        Console.WriteLine();
     }
 }
